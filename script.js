f01173ac2b1099cd71ea73e591ae5719fb8d7673
kuriroley <christian.rodriguez@tutanota.com>
2023-08-03 09:32:09 +0900

    working on script

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const container = document.getElementById('etch-container');
 
 
 function tileSorter(){
-    for (let i = 0; i< 16; i++){
-    const tile = document.createElement('div');
-    tile.classList.add('tile');
-    container.appendChild(tile);
+    for (let i = 0; i < 16; i++){
+        for (let j = 0; j < 16; j++){
+        const tile = document.createElement('div');
+        tile.classList.add('tile');
+        container.appendChild(tile);
+        }
     }
 }
 tileSorter();
