9854c0d776ff6450067585d6769f56de1cf6416b
kuriroley <christian.rodriguez@tutanota.com>
2023-08-04 16:07:19 +0900

    changes were made

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const container = document.getElementById('etch-container');
 
+        
 
 function tileSorter(){
-    for (let i = 0; i < 16; i++){
-        for (let j = 0; j < 16; j++){
-        const tile = document.createElement('div');
-        tile.classList.add('tile');
+
+    for (let i = 0; i < 8; i++){
+        for (let j = 0; j < 8; j++){
+            const tile = document.createElement('div');
+            tile.classList.add('tile');
         container.appendChild(tile);
         }
     }
