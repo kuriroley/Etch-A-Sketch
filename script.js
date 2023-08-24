a8e6ea6bb92473af002dfe16b15de77b036cbf01
kuriroley <christian.rodriguez@tutanota.com>
2023-08-24 18:39:03 +0900

    made more changes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const setSizeButton = document.querySelector('.set-size-button');
 let click = true;
 setSizeButton.addEventListener('click', () =>{
-    let sketchSize = prompt('enter a size between 1 and 100');
+    let sketchSize = prompt('Please enter a value between 2 and 100');
     if (sketchSize >= 2 && sketchSize <= 100){
         sketchStarter(sketchSize);
     } else {
@@ -10,13 +10,14 @@ setSizeButton.addEventListener('click', () =>{
     
 })
 
-
 function sketchStarter(value){
     const resetButton = document.querySelector('.reset');
     const container = document.querySelector('.sketch-pad');
     const textContainer = document.querySelector('.text-container');
     const currentSizeText = document.createElement('p');
     const randomizeColor = document.querySelector('.randomize-color');
+    const blackColor = document.querySelector('.black-color');
+    const eraser = document.querySelector('.eraser');
     container.style.gridTemplateRows = `repeat(${value}, 1fr)`;
     container.style.gridTemplateColumns = `repeat(${value}, 1fr)`;
 
@@ -31,20 +32,29 @@ function sketchStarter(value){
         });
         resetButton.addEventListener('click', () =>{
             tile.style.backgroundColor = 'white';
-            currentSizeText.textContent = '';
         });
         randomizeColor.addEventListener('click', ()=>{
             tile.addEventListener('mouseover', () =>{
                 tile.style.backgroundColor = randomColor();
             });
+        });
+        blackColor.addEventListener('click', ()=>{
+            tile.addEventListener('mouseover', ()=>{
+                tile.style.backgroundColor = 'black';
+            });
+        });
+        eraser.addEventListener('click', ()=>{
+            tile.addEventListener('mouseover', ()=>{
+                tile.style.backgroundColor = 'white';
+            })
         })
 
     }
     
 }
-
+// study why it works
 function randomColor(){
-    var color = Math.floor(Math.random() * 16777216).toString(16);
+    let color = Math.floor(Math.random() * 16777216).toString(16);
     return '#000000'.slice(0, -color.length) + color;
 }
 sketchStarter(16);
